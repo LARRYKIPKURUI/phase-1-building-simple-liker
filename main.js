9bc37db0b911da76455618b3c22cae4c1e25b639
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-18 23:57:08 +0300

    liker app

--- main.js
+++ main.js
@@ -6,6 +6,41 @@ const FULL_HEART = '♥'
 
 
 
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = document.querySelectorAll(".like-glyph"); // Select all hearts
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // check between empty and full hearts
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART;
+            heart.classList.add("activated-heart");
+          } else {
+            heart.textContent = EMPTY_HEART;
+            heart.classList.remove("activated-heart");
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          
+          // Display the error message
+          const errorElement = document.querySelector("#error-message");
+          errorElement.textContent = error;
+          errorElement.classList.remove("hidden"); // Make it visible
+
+          // Hide the error message after 3 seconds (3000 ms)
+          setTimeout(() => {
+            errorElement.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
